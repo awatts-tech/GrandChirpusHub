e7e4ded6e91fcf55d7ce8c8d90ffd9eb43f53e91
trathbun <rathbun.tiffany@gmail.com>
2021-03-03 21:59:41 -0500

    functioning API

--- grandchirpus/src/app/indeed.service.ts
+++ grandchirpus/src/app/indeed.service.ts
@@ -1,11 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface JobSearch {
+  jobTitle: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class IndeedService {
+
+  public jobsList: JobSearch[] = [];
   userID = "vDMCdyYaOSf6Ytb";
-  tokenKey = "780dJANrAPBguLXOcJzBSNLBKrqqiKt4O6PKwbd4Z33EedMhEBZnU4vxtbysDraVqWcOF3EeRMYjuxc7kaqsC+x8Q==ebef6";
+  tokenKey = "dJANrAPBguLXOcJzBSNLBKrqqiKt4O6PKwbd4Z33EedMhEBZnU4vxtbysDraVqWcOF3EeRMYjuxc7kaqsC+x8Q==";
   url = "https://api.careeronestop.org/v1/jobsearch/";
+  
+  // const keyword = "developer";
+  // const location = "United States";
+  // const radius = 25;
+  // const days = 0;
+  // const sortColumns = 0;
+  // const sortOrder = 0;
+  // const startRecord = 0; 
+  // const pageSize = 10; 
+
   constructor(private http: HttpClient) { }
-}
+  
+  getJobs() {
+
+    const requestUrl = this.getUrlWithAPIKey();
+
+    this.http.get(requestUrl, {headers: {Authorization: 'Bearer ' + this.tokenKey, "Content-Type": "application/json"}}).subscribe(
+      
+      (response: any) => {
+
+        console.log(response);
+
+      },
+      (error) => {
+        console.error(error);
+      }
+      
+    );
+  }
+  getUrlWithAPIKey() {
+    return `${this.url}${this.userID}/developer/United States/25/0/0/0/10/0`;
+  }
+    
+  }
