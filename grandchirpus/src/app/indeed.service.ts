89654b58df7b34c5ceeb1135704db1d05b88fbfb
Asia Watts <wattsasiak@gmail.com>
2021-03-03 20:00:19 -0500

    updated files

--- grandchirpus/src/app/indeed.service.ts
+++ grandchirpus/src/app/indeed.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class IndeedService {
-
-  constructor() { }
+  userID = "vDMCdyYaOSf6Ytb";
+  tokenKey = "780dJANrAPBguLXOcJzBSNLBKrqqiKt4O6PKwbd4Z33EedMhEBZnU4vxtbysDraVqWcOF3EeRMYjuxc7kaqsC+x8Q==ebef6";
+  url = "https://api.careeronestop.org/v1/jobsearch/";
+  constructor(private http: HttpClient) { }
 }
