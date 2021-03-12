3a26e4eccab18583b0d5723704d4d1f063209a60
Ferenccx <69224300+Ferenccx@users.noreply.github.com>
2021-03-11 22:13:48 -0700

    techquestions

--- grandchirpus/src/app/interview-prep/interview-prep.component.ts
+++ grandchirpus/src/app/interview-prep/interview-prep.component.ts
@@ -11,7 +11,7 @@ export class InterviewPrepComponent implements OnInit {
   }
    basicQuestions = [
     "Tell me about yourself",
-    "What is your greatest strengh?",
+    "What is your greatest strength?",
     "What is your greatest weakness?",
     "Why should we hire you?",
     "Why do you want to work here?",
@@ -20,15 +20,15 @@ export class InterviewPrepComponent implements OnInit {
     "What would your co-workers say about you?",
     ];
    technicalQuestions : string[] = [
-      "What are ways your html can give responsive design",
-      "You only live once, but if you do it right, once is enough. - Mae West",
-      "Be the change that you wish to see in the world. - Mahatma Gandhi",
-      "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty. – Winston Churchill",
-      "Don’t Let Yesterday Take Up Too Much Of Today. – Will Rogers",
-      "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character. – Unknown",
-      "Its Not Whether You Get Knocked Down, It’s Whether You Get Up.  - Vince Lombardi",
-      "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You. – Steve Jobs",
-      "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do. – Rob Siltane",
+      "How Would You Explain APIs to a Nontechnical Stakeholder?",
+      "Tell Me About a Complex Application You’ve Worked on or Supported.",
+      "How are you defining your media queries? With min-width or max-width? What is the difference between the two?",
+      "Explain component based architecture. What is it? What is a component? Show an example",
+      "What’s a New Software or Technology You Recently Worked With and How Did You Come to Learn It?",
+      "Tell Me About a Time When You Explained a Technical Process or Concept to Someone Who Didn’t Have a Technical Background.",
+      "What development tools have you used?",
+      "What languages have you programmed in?",
+      "What strengths do you think are most important in a developer [or another relevant IT position]?",
     ];
     
      basicRandom(){
