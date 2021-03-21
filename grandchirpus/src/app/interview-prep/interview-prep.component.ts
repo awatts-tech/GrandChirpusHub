74e34f1f93ecee1d35f0375bad6a19ef9e0000fb
Ferenccx <69224300+Ferenccx@users.noreply.github.com>
2021-03-20 19:37:17 -0600

    fixanswers

--- grandchirpus/src/app/interview-prep/interview-prep.component.ts
+++ grandchirpus/src/app/interview-prep/interview-prep.component.ts
@@ -46,26 +46,16 @@ export class InterviewPrepComponent implements OnInit {
 
 
     technicalAnswers : string[] = [
-
-      // Entering dummy info to test
-
-      "I started with the company 8 years ago. I have been on my current team for x years",
-      // "Here's one way this might sound:If I were explaining what APIs are to someone without a lot of technical background, I’d probably say
-      // that at their core, APIs are a way for programs to communicate with each other—usually with a limited set of possible inputs and responses 
-      // like a menu at a restaurant (where the inputs are menu items and the responses are food). But unlike a menu, you have to use the exact 
-      // keywords an API is coded for or it won’t understand you. For example, if I use the correct inputs, Instagram’s API allows me to code a 
-      // website so that the company’s most recent Instagram posts are automatically displayed on their site",
-      // "When responding to this question, explain why you considered the application(s) complex, then describe your role working on them. Were you involved in the front- or back-end development, testing, design, or maintenance? Were you the sole developer or part of a development team?
-      // From there, focus on a specific example of something you worked on and take a few minutes to summarize what you were tasked with, the actions you took to accomplish those tasks, and how it fit into the overall project. Be sure to include what you learned and how the experience shaped you as a professional. While you don’t want to drone on and on, it’s important to include some specific details to keep your response from being too general or generic.
-      // ",
-      // "Media queries are a popular technique for delivering a tailored style sheet to different devices. Min-width is uses mobile view <800 (based on your code parameter) Max width was showing mobile and tablet view >800",
-      // "Component-based architecture focuses on the decomposition of the design into individual functional or logical components that represent well-defined communication interfaces containing methods, events, and properties. It provides a higher level of abstraction and divides the problem into sub-problems, each associated with component partitions.A component is a modular, portable, replaceable, and reusable set of well-defined functionality that encapsulates its implementation and exporting it as a higher-level interface.",
-      // "You’ll want to stress to your interviewer how you acquired your skills, whether through school, vocational training, certification, previous jobs, or a combination of these. If you had the opportunity to pick up new software knowledge or skills as a result of a project you worked on, this question offers a great opportunity to share that and explain how you’ve used the skills in practice.
-      //  “I’m interested in knowing how candidates apply what they’ve learned, not that they simply have the knowledge,” says Brooks. With that in mind, you’ll want to be sure to share how you’ve used the technology tools you’re familiar with.",
-      //  "Tell Me About a Time When You Explained a Technical Process or Concept to Someone Who Didn’t Have a Technical Background.",
-      //  "What development tools have you used?",
-      //  "What languages have you programmed in?",
-      //  "What strengths do you think are most important in a developer [or another relevant IT position]?",
+      
+      "If I were explaining what APIs are to someone without a lot of technical background, I’d probably say that at their core, APIs are a way for programs to communicate with each other—usually with a limited set of possible inputs and responses like a menu at a restaurant (where the inputs are menu items and the responses are food). But unlike a menu, you have to use the exact keywords API is coded for or it won’t understand you. For example, if I see the correct inputs, Instagram’s API allows me to code a website so that the company’s most recent Instagram posts are automatically displayed on their site",
+      "When responding to this question, explain why you considered the application(s) complex, then describe your role working on them. Were you involved in the front or back-end development, testing, design, or maintenance? Were you the sole developer or part of a development team? From there, focus on a specific example of something you worked on and take a few minutes to summarize what you were tasked with, the actions you took to accomplish those tasks, and how it fit into the overall project. Be sure to include what you learned and how the experience shaped you as a professional. While you don’t want to drone on and on, it’s important to include some specific details to keep your response from being too general or generic",
+      "Media queries are a popular technique for delivering a tailored style sheet to different devices. Min-width is uses mobile view <800 (based on your code parameter) Max width was showing mobile and tablet view >800",
+      "Component-based architecture focuses on the decomposition of the design into individual functional or logical components that represent well-defined communication interfaces containing methods, events, and properties. It provides a higher level of abstraction and divides the problem into sub-problems, each associated with component partitions.A component is a modular, portable, replaceable, and reusable set of well-defined functionality that encapsulates its implementation and exporting it as a higher-level interface.",
+      "You’ll want to stress to your interviewer how you acquired your skills, whether through school, vocational training, certification, previous jobs, or a combination of these. If you had the opportunity to pick up new software knowledge or skills as a result of a project you worked on, this question offers a great opportunity to share that and explain how you’ve used the skills in practice",
+      "If you are trying to explain a technical concept be aware of who your audience is and tune your technical speak to their level. There might be times where you'll replace the technical talk with something that your audience will understand, remember that your ultimate goal is to communicate and to do it clearly",
+      "Software Development Tools that will make you more productive - GitHub - The world's leading software development platform, GitLab, IntelliJ IDEA, Stack Overflow, Docker, Jira, Jenkins",
+      "Example: C++ was the first programming language in an academic class. Before I went to college, though, I had picked up a fair amount of JavaScript and Python. I remember making my first math-based game with JavaScript. I still enjoy using those languages because they’re flexible enough that you can use them in a lot of contexts",
+      "7 Qualities That Differentiate a Great Programmer from a Good Programmer Positive Attitude. A great programmer cares about your product's success, Supreme Communication Skills, Good Time and Task Management, Quick Learning Ability, Deep and Broad Technical Experience, A Good Team Player, Big-Picture Focus",
     ];
     
     
